95cf66ab558275b0103d93a6582178628171f02c
jvmho <ch0pp3212@gmail.com>
2026-04-12 13:26:15 +0600

    fix: fixed API endpoints with carts and cart items

--- backend/pharmacy-api/cmd/app/main.go
+++ backend/pharmacy-api/cmd/app/main.go
@@ -37,8 +37,10 @@ func main() {
 	http.HandleFunc(base+"/batches/", api.Auth(batchHandler.Get))
 	http.HandleFunc(base+"/batches", api.Auth(batchHandler.List))
 	http.HandleFunc(base+"/stock", api.Auth(stockHandler.List))
-	http.HandleFunc(base+"/cart", api.Auth(cartHandler.Get))
-	http.HandleFunc(base+"/cart/item", api.Auth(cartHandler.AddItem))
+	http.HandleFunc(base+"/cart", api.Auth(cartHandler.List))      // GET /cart и GET /cart?userId=
+	http.HandleFunc(base+"/cart/", api.Auth(cartHandler.Router))   // GET /cart/{id} и GET /cart/{id}/items
+	http.HandleFunc(base+"/cart/item", api.Auth(cartHandler.AddItem))    // POST /cart/item
+	http.HandleFunc(base+"/cart/items/", api.Auth(cartHandler.DeleteItem)) // DELETE /cart/items/{id}
 	http.HandleFunc(base+"/orders", api.Auth(orderHandler.Create))
 	http.HandleFunc(base+"/orders/", api.Auth(orderHandler.Get))
 	http.HandleFunc(base+"/supplies", api.Auth(supplyHandler.List))
